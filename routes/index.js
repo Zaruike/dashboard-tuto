1cddf0bacadb66efdfb2b43e06ce68e683e9649f
Joris Vidéo <jorisvideo@outlook.fr>
2017-10-15 20:49:29 +0200

    tuto sur le dashboard

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', ensureAuthenticated, function(req, res){
+	res.render('index');
+});
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		req.flash('error_msg','You are not logged in');
+		res.redirect('/users/login');
+	}
+}
+
+module.exports = router;
